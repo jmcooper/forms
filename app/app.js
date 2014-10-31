bb77565cbd6b5e497d7c7691c1a0123335ac2405
Jim Cooper <jim-cooper@pluralsight.com>
2014-10-30 23:51:58 -0600

    Moved bower components.  Installed bootstrap.  Added server.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,2 @@
 'use strict';
-var formsModule = angular.module('forms', ['ngRoute'])
-    .config(function($routeProvider, $locationProvider) {
-        $routeProvider.when('', { templateUrl: ''});
-
-        $locationProvider.html5Mode(true);
-    });
+var formsModule = angular.module('forms', []);
